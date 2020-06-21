b66ffee06cbe29edfde91767ac9d757890c4ce9f
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2020-06-21 21:27:40 +0100

    Add privacy policy link to the footer

--- conf.py
+++ conf.py
@@ -902,9 +902,9 @@ LICENSE = ""
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
 CONTENT_FOOTER = '''
-Contents &copy; {date}
-<a href="mailto:{email}">{author}</a> -
-Powered by <a href="https://getnikola.com" rel="nofollow">Nikola</a>{license}
+Contents &copy; {date} <a href="mailto:{email}">{author}</a> {license} |
+<a href="/en/pages/privacy-policy/">Privacy Policy</a> |
+Powered by <a href="https://getnikola.com">Nikola</a>
 '''
 
 # Things that will be passed to CONTENT_FOOTER.format().  This is done
