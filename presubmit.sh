0ee50aa7b70a68e5a5bc8ef78512b25fffbf978e
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-06-13 20:22:12 +0100

    Move sast reports to output

--- presubmit.sh
+++ presubmit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pytype .
-docker run --rm -e "WORKSPACE=${PWD}" -e "ENABLE_OSS_RISK=true" -v "$PWD:/app" --user $UID:$GID shiftleft/sast-scan scan
+docker run --rm -e "WORKSPACE=${PWD}" -e "ENABLE_OSS_RISK=true" -v "$PWD:/app" --user $UID:$GID shiftleft/sast-scan scan --out_dir /app/output/sast --build
