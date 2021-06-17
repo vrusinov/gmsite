ad0d78c5e7e7fa82c277929631d717817a4ffaba
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-06-17 22:52:27 +0100

    Clean sast directory before running it again

--- presubmit.sh
+++ presubmit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 pytype .
+rm -rf output/sast
 docker run --rm -e "WORKSPACE=${PWD}" -e "ENABLE_OSS_RISK=true" -v "$PWD:/app" --user $UID:$GID shiftleft/sast-scan scan --out_dir /app/output/sast --build
