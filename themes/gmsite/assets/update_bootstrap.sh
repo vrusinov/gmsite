c2b1c8c087dd4b9b7f9247ca66a283133f7f1cd5
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2023-11-03 21:51:04 +0000

    Update bootstrap to 5.3.2

--- themes/gmsite/assets/update_bootstrap.sh
+++ themes/gmsite/assets/update_bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="5.3.1"
+VERSION="5.3.2"
 
 wget https://cdn.jsdelivr.net/npm/bootstrap@${VERSION}/dist/css/bootstrap.min.css -O css/bootstrap.min.css
 wget https://cdn.jsdelivr.net/npm/bootstrap@${VERSION}/dist/js/bootstrap.min.js -O js/bootstrap.min.js
