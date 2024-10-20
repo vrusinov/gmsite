f3b780ac1537aa08f6d305f108e9adc2c8e95c29
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2024-10-20 23:05:55 +0100

    Remove author name

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import time
 from typing import Any, Callable, Dict, List
 
 # Data about this site
-BLOG_AUTHOR = "V Rusinov"  # (translatable)
+BLOG_AUTHOR = "Vladimir Rusinov"  # (translatable)
 BLOG_TITLE = {
     'ru': "www.rusinov.ie",
     'en': 'www.rusinov.ie'
